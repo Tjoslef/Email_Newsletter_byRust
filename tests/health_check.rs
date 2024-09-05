2857d03d7c850eeae571bc770b545938f7731f13
Tjoslef <josef.pasek17@gmail.com>
2024-09-06 00:18:46 +0200

    update 6.9

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,14 @@
 //! tests/health_check.rs
-//use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::run;
+
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
 
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
@@ -29,8 +30,12 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let _connection = PgConnection::connect(&connection_string)
+    .await
+    .expect("Failed to connect to Postgres");
     let client = reqwest::Client::new();
-
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
